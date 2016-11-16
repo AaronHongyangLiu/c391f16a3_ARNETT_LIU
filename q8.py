970d3a00926f09351bb92f13f82043c0a9e8f2a6
tdarnett <tarnett1@mac.com>
2016-11-16 00:36:15 -0700

    added logic to parse lines

--- q8.py
+++ q8.py
@@ -1,8 +1,16 @@
-import sqlite3 #https://docs.python.org/2/library/sqlite3.html
+import sqlite3  # https://docs.python.org/2/library/sqlite3.html
 import sys
 import re
 
-PREDICATE_URL = {}
+PREFIX_URL = {}
+
+STATE = {
+    'IN_SUBJECT' : False,
+    'IN_PREDICATE' : False,
+    'IN_OBJECT' : False
+}
+
+
 
 def main():
     if len(sys.argv) != 3:
@@ -28,16 +36,89 @@ def parse_file(db, lines):
     param db: the database instance
     param lines: the list of lines read from RDF data file
     """
+    subject = None
+    predicate = None
+    object = None
 
     for line in lines:
-        # print line
-        prefix = re.search("(?:prefix)(.*)", line) #TODO use regular expressions as much as possible for string manipulations
-        #https://docs.python.org/2/howto/regex.html
-        print prefix.group()
-        break
-        # line_contents = line.split('\t')
-        # if line_contents[0] == ''
-        # print line_contents
+        # check if prefix line
+        prefix = re.search("(?:)@prefix(.*)", line)  # https://docs.python.org/2/howto/regex.html
+        if prefix:
+            add_prefix(prefix)
+            continue # go to next line
+        line_contents = line.split('\t')
+
+
+
+
+        # if not STATE['IN_SUBJECT']: # Then new triple
+        if line_contents[0] != '':  # then subject field available
+            subject = generate_attribute(line_contents[0])
+            STATE['IN_SUBJECT'] = True
+            predicate = generate_attribute(line_contents[1])
+
+            if line_contents[2][-2] == ',':
+                object_with_prefix = re.search("^[^,]*",line_contents[2]).group() # regex from http://stackoverflow.com/questions/19142042/python-regex-to-get-everything-until-the-first-dot-in-a-string
+                object = generate_attribute(object_with_prefix)
+                insert_triple(db, subject, predicate, object)
+            if line_contents[2][-2] == '.': # -2 index because last character is newline character
+                object_with_prefix = re.search("^[^.]*", line_contents[
+                    2]).group()  # regex from http://stackoverflow.com/questions/19142042/python-regex-to-get-everything-until-the-first-dot-in-a-string
+                object = generate_attribute(object_with_prefix)
+                insert_triple(db, subject, predicate, object)
+                subject = None
+                predicate = None
+                object = None
+        elif line_contents[0] == '' and line_contents[1] == '' and subject and predicate:
+            object_with_prefix = re.search("^[^,]*", line_contents[
+                2]).group()  # regex from http://stackoverflow.com/questions/19142042/python-regex-to-get-everything-until-the-first-dot-in-a-string
+            object = generate_attribute(object_with_prefix)
+            insert_triple(db, subject, predicate, object)
+
+
+
+        # if STATE['IN_SUBJECT']:
+        #     print line_contents
+
+
+
+
+def generate_attribute(tag):
+    """
+    generates the text that will be saved in database
+    :param tag: example -->  dbr:Edmonton
+    :return: the tag without prefixes
+    """
+    tag_contents = tag.split(':')
+    print tag
+    result = "%s%s" % (PREFIX_URL[tag_contents[0]], tag_contents[1])
+    return result
+
+def add_prefix(prefix):
+    prefix_contents = prefix.group(1).split('\t')
+
+    # this gets the url of the prefix tag. http://stackoverflow.com/questions/4894069/regular-expression-to-return-text-between-parenthesis
+    url = prefix_contents[1][prefix_contents[1].find("<") + 1:prefix_contents[1].find(
+        ">")]
+
+    PREFIX_URL[prefix_contents[0].strip()[:-1]] = url  # Add to the PREFIX_URL dictionary
+
+def insert_triple(db, subject, predicate, object):
+    print '\n'
+
+    print subject
+    print predicate
+    print object
+    print '\n'
+
+    # TODO insert into the DB
+    return
+
+# def new_triple():
+#     """
+#     :return: true if not currently parsing a previous triple
+#     """
+#     return (not STATE['IN_OBJECT'] and not STATE['IN_PREDICATE'] and not STATE['IN_SUBJECT'])
 
 if __name__ == "__main__":
     main()
